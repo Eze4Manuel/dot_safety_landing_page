b98b1cade53a333b9fdfd6d10b867e6c74669d82
Prophet-Bestman <bestiproph@gmail.com>
2022-01-12 06:39:48 +0100

    Minor update

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
     <div
       className={`max-w-[360px] min-h-[250px] bg-white drop-shadow-lg py-12 px-8 rounded-[10px] ${className}`}
     >
-      <img src={icon} />
+      <img src={icon} alt="" />
       <div className="my-3">
         <h4 className="text-xl text-grey-800 font-semibold capitalize">
           {title}
